05e0905a7e476150cc5d3abd6b6de7565064a22a
parklize <parklize@gmail.com>
2024-07-11 12:45:21 +0100

    testing NiFi1.21.1

--- server/sh/start.sh
+++ server/sh/start.sh
@@ -127,8 +127,8 @@ case ${AUTH} in
 esac
 
 # Overwrite keystore/truststore and nifi.properties
-cp ../custom/*.jks ${NIFI_HOME}/conf
-cp ../custom/nifi.properties ${NIFI_HOME}/conf
+#cp ../custom/*.jks ${NIFI_HOME}/conf
+#cp ../custom/nifi.properties ${NIFI_HOME}/conf
 
 # Continuously provide logs so that 'docker logs' can produce them
 "${NIFI_HOME}/bin/nifi.sh" run &
