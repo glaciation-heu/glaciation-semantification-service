e13c2f45d00ba3bcf6801d32d2bb75a388171e6e
parklize <parklize@gmail.com>
2024-07-11 14:27:02 +0100

    update start scritp to turn off HTTPS

--- server/sh/start.sh
+++ server/sh/start.sh
@@ -47,7 +47,7 @@ prop_replace 'nifi.remote.input.secure'         'false'
 prop_replace 'nifi.remote.input.http.enabled'   'true'
 #prop_replace 'nifi.cluster.protocol.is.secure'  'false'
 # Test
-prop_replace 'nifi.web.https.host'		'0.0.0.0'
+prop_replace 'nifi.web.http.host'		'0.0.0.0'
 prop_replace 'nifi.web.http.port'       '8080'
 
 # Set nifi-toolkit properties files and baseUrl
